cda870dad944bf6ec5721b5cd2ae7de8be2c5b2e
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-03-08 13:03:54 +0100

    Rename GreetingModule to GreetingComponent

--- androidApp/src/main/java/com/fredporciuncula/inject/greeter/android/ApplicationComponent.kt
+++ androidApp/src/main/java/com/fredporciuncula/inject/greeter/android/ApplicationComponent.kt
@@ -2,7 +2,7 @@ package com.fredporciuncula.inject.greeter.android
 
 import android.content.Context
 import com.fredporciuncula.inject.greeter.GreetingHandler
-import com.fredporciuncula.inject.greeter.GreetingModule
+import com.fredporciuncula.inject.greeter.GreetingComponent
 import com.fredporciuncula.inject.greeter.Singleton
 import me.tatarka.inject.annotations.Component
 import me.tatarka.inject.annotations.Provides
@@ -10,7 +10,7 @@ import me.tatarka.inject.annotations.Provides
 @Component @Singleton
 abstract class ApplicationComponent(
   @get:Provides val context: Context,
-) : GreetingModule {
+) : GreetingComponent {
   abstract val greetingHandlerCreator: (String) -> GreetingHandler
 }
 
