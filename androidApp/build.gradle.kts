9b82e0c13f39ebcbb13a9a91f76d28ad4ff2145a
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-03-08 12:46:44 +0100

    Replace Hilt dependencies with kotlin-inject

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,8 +1,7 @@
 plugins {
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.android.application)
-  alias(libs.plugins.kapt)
-  alias(libs.plugins.hilt)
+  alias(libs.plugins.ksp)
 }
 
 android {
@@ -32,10 +31,6 @@ android {
   }
 }
 
-kapt {
-  correctErrorTypes = true
-}
-
 dependencies {
   implementation(project(":shared"))
   implementation(libs.androidx.activity.compose)
@@ -47,6 +42,6 @@ dependencies {
   implementation(libs.compose.ui.toolingPreview)
   debugRuntimeOnly(libs.compose.ui.tooling)
 
-  implementation(libs.hilt)
-  kapt(libs.hilt.compiler)
+  implementation(libs.kotlinInject.runtime)
+  ksp(libs.kotlinInject.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ plugins {
   alias(libs.plugins.kotlin.android).apply(false)
   alias(libs.plugins.android.application).apply(false)
   alias(libs.plugins.android.library).apply(false)
-  alias(libs.plugins.kapt).apply(false)
-  alias(libs.plugins.hilt).apply(false)
 }
 
 tasks.register("clean", Delete::class) {
